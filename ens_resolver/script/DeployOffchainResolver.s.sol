a43abc28917f2ebe802ccbc0e097096ab5a13aa6
Sparsh Agarwal <sparshaga25@gmail.com>
2025-09-27 18:50:37 +0530

    Add initial implementation of OffchainResolver and related contracts, including factory, signature verifier, and interface definitions

---
+++ ens_resolver/script/DeployOffchainResolver.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+
+import "../src/OffchainResolver.sol";
+
+contract DeployOffchainResolver is Script {
+    uint256 internal deployerKey;
+    string internal resolverURL;
+    address[] internal signerList;
+
+    function setUp() public {
+        deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        resolverURL = vm.envString("RESOLVER_URL");
+        signerList = _loadSigners();
+    }
+
+    function run() external returns (OffchainResolver resolver) {
+        setUp();
+
+        vm.startBroadcast(deployerKey);
+        resolver = new OffchainResolver();
+        resolver.initialize(resolverURL, signerList);
+        vm.stopBroadcast();
+
+        console2.log("OffchainResolver deployed to", address(resolver));
+        console2.log("Resolver URL", resolverURL);
+        console2.log("Signer count", signerList.length);
+    }
+
+    function _loadSigners() internal returns (address[] memory signers) {
+        string memory signersJson = vm.envOr("RESOLVER_SIGNERS", string("[]"));
+        string memory wrapped = string.concat('{"signers":', signersJson, "}");
+        signers = vm.parseJsonAddressArray(wrapped, ".signers");
+    }
+}
